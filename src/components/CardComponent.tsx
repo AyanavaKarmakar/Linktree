25f250adea4f5926cd42bbcb6fb6d368213ca323
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2022-10-13 00:38:24 +0530

    feat: create CardComponent

---
+++ src/components/CardComponent.tsx
@@ -0,0 +1,9 @@
+export const CardComponent = () => {
+  return (
+    <div className="card glass card-bordered card-compact w-80">
+      <div className="card-body">
+        <h2 className="card-title text-cyan-200">Link</h2>
+      </div>
+    </div>
+  );
+};
