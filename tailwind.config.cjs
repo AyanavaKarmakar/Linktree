2d43eadb66602379a41c7fe15ab16be9565a539e
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2022-10-12 20:44:46 +0530

    feat: add theme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,4 +5,22 @@ module.exports = {
     extend: {},
   },
   plugins: [require("@tailwindcss/typography"), require("daisyui")],
+  daisyui: {
+    logs: true,
+    themes: [
+      {
+        mytheme: {
+          primary: "#c5d9f9",
+          secondary: "#d3ea88",
+          accent: "#fcb8ba",
+          neutral: "#211B28",
+          "base-100": "#383838",
+          info: "#2F4FD0",
+          success: "#54D990",
+          warning: "#E28B08",
+          error: "#FD6877",
+        },
+      },
+    ],
+  },
 };
