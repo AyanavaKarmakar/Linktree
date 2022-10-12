4f0c741eb4e69de31782c33809d5ebd7fb8023be
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2022-10-13 00:01:21 +0530

    feat: create LinkTreeComponent

---
+++ src/components/LinkTreeComponent.tsx
@@ -0,0 +1,12 @@
+export const LinkTreeComponent = () => {
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <ul className="steps steps-vertical">
+        <li className="step step-primary text-cyan-50">Link 2</li>
+        <li className="step step-primary text-cyan-50">Link 1</li>
+        <li className="step step-primary text-cyan-50">Link 3</li>
+        <li className="step step-primary text-cyan-50">Link 4</li>
+      </ul>
+    </div>
+  );
+};
