22b64cd83a579e0f8745d435d92fb1bd814aeffa
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2022-10-13 00:01:58 +0530

    feat: import LinkTreeCompoent

--- src/components/HeroComponent.tsx
+++ src/components/HeroComponent.tsx
@@ -1,3 +1,5 @@
+import { LinkTreeComponent } from "./LinkTreeComponent";
+
 export const HeroComponent = () => {
   return (
     <div
@@ -6,7 +8,9 @@ export const HeroComponent = () => {
         backgroundImage: `url("https://user-images.githubusercontent.com/89210438/194807936-f30efc94-0832-40f6-ae3d-38cd2f6c04a8.png")`,
       }}
     >
-      <div className="hero-overlay bg-opacity-60"></div>
+      <div className="hero-overlay bg-opacity-90">
+        <LinkTreeComponent />
+      </div>
     </div>
   );
 };
