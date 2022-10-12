480a8dcb230d92361df4a2eafe34cabdf6cde350
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2022-10-12 23:46:04 +0530

    feat: add Footer component

---
+++ src/components/FooterComponent.tsx
@@ -0,0 +1,9 @@
+export const FooterComponent = () => {
+  return (
+    <footer className="footer footer-center bg-base-300 p-4 text-base-content">
+      <div>
+        <p>Copyright © 2022 - All right reserved by Ayanava Karmakar</p>
+      </div>
+    </footer>
+  );
+};
