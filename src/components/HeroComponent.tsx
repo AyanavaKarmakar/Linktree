a9f5ec64875b05ed0a9b0adfa6becfc7d1b88413
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2022-10-12 23:40:38 +0530

    feat: create Hero component

---
+++ src/components/HeroComponent.tsx
@@ -0,0 +1,12 @@
+export const HeroComponent = () => {
+  return (
+    <div
+      className="hero min-h-screen"
+      style={{
+        backgroundImage: `url("https://user-images.githubusercontent.com/89210438/194807936-f30efc94-0832-40f6-ae3d-38cd2f6c04a8.png")`,
+      }}
+    >
+      <div className="hero-overlay bg-opacity-60"></div>
+    </div>
+  );
+};
