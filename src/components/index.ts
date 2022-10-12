59e50fdbc8ea0ef5b07e8973f35142683e029caf
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2022-10-13 00:38:46 +0530

    feat: setup export for card component

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,4 @@ export { NavbarComponent } from "./NavbarComponent";
 export { HeroComponent } from "./HeroComponent";
 export { FooterComponent } from "./FooterComponent";
 export { LinkTreeComponent } from "./LinkTreeComponent";
+export { CardComponent } from "./CardComponent";
