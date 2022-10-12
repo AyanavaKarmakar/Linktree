f732129f6fa36262e6dc5c7a4118a14c095dc26e
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2022-10-13 00:01:38 +0530

    feat: export LinkTreeComponent

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export { NavbarComponent } from "./NavbarComponent";
 export { HeroComponent } from "./HeroComponent";
 export { FooterComponent } from "./FooterComponent";
+export { LinkTreeComponent } from "./LinkTreeComponent";
