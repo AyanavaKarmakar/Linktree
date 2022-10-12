f745850576e7434523012e8170fe0ece4270ca37
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2022-10-12 22:50:00 +0530

    feat: add basic navbar

--- src/components/NavbarComponent.tsx
+++ src/components/NavbarComponent.tsx
@@ -1,3 +1,9 @@
 export const NavbarComponent = () => {
-  return <>Navbar</>;
+  return (
+    <div className="navbar bg-gradient-to-r from-indigo-600 bg-auto text-primary-content">
+      <h2 className="font-mono text-3xl font-bold tracking-wide text-cyan-200  subpixel-antialiased">
+        ELFIN
+      </h2>
+    </div>
+  );
 };
