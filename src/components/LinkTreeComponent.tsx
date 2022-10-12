5e66be934cc6836ad0bbd818575bae706755a194
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2022-10-13 00:39:23 +0530

    feat: use card component

--- src/components/LinkTreeComponent.tsx
+++ src/components/LinkTreeComponent.tsx
@@ -1,11 +1,18 @@
+import { CardComponent } from "./CardComponent";
+
 export const LinkTreeComponent = () => {
   return (
     <div className="flex h-screen items-center justify-center">
       <ul className="steps steps-vertical">
-        <li className="step step-primary text-cyan-50">Link 2</li>
-        <li className="step step-primary text-cyan-50">Link 1</li>
-        <li className="step step-primary text-cyan-50">Link 3</li>
-        <li className="step step-primary text-cyan-50">Link 4</li>
+        <li className="step step-primary mb-10 text-cyan-50">
+          <CardComponent />
+        </li>
+        <li className="step step-primary mb-10 text-cyan-50">
+          <CardComponent />
+        </li>
+        <li className="step step-primary mb-10 text-cyan-50">
+          <CardComponent />
+        </li>
       </ul>
     </div>
   );
