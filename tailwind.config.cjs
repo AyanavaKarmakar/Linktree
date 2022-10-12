c3d5bbf36d82485b6230801c948f050403b50d7c
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2022-10-12 22:49:25 +0530

    feat: remove theme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,20 +7,5 @@ module.exports = {
   plugins: [require("@tailwindcss/typography"), require("daisyui")],
   daisyui: {
     logs: true,
-    themes: [
-      {
-        mytheme: {
-          primary: "#c5d9f9",
-          secondary: "#d3ea88",
-          accent: "#fcb8ba",
-          neutral: "#211B28",
-          "base-100": "#383838",
-          info: "#2F4FD0",
-          success: "#54D990",
-          warning: "#E28B08",
-          error: "#FD6877",
-        },
-      },
-    ],
   },
 };
