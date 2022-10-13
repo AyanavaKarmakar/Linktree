5c24e85f399ecdd9359d239da4de135a72c3f35e
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2022-10-13 15:46:01 +0530

    feat: map `linkURL` to button

--- src/components/CardComponent.tsx
+++ src/components/CardComponent.tsx
@@ -1,12 +1,23 @@
+import { ArrowRedirect } from "../utils/icons";
 import { LinkType } from "../utils/links";
 
 export const CardComponent = (props: LinkType) => {
-  const { linkName } = props;
+  const { linkName, linkURL } = props;
 
   return (
-    <div className="card glass card-bordered card-compact w-80">
-      <div className="card-body">
+    <div className="justify-cente card glass card-bordered w-80 items-center">
+      <div className="card-title">
         <h2 className="text-base-0 card-title">{linkName}</h2>
+        <div className="card-actions">
+          <a
+            href={linkURL}
+            target="_blank"
+            rel="noreferrer"
+            className="btn btn-ghost btn-circle"
+          >
+            <ArrowRedirect />
+          </a>
+        </div>
       </div>
     </div>
   );
