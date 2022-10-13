3abce0ddf8c102e99c8abec771daa4d450ced593
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2022-10-13 15:24:00 +0530

    feat: map link props to cards

--- src/components/CardComponent.tsx
+++ src/components/CardComponent.tsx
@@ -1,8 +1,12 @@
-export const CardComponent = () => {
+import { LinkType } from "../utils/links";
+
+export const CardComponent = (props: LinkType) => {
+  const { linkName } = props;
+
   return (
     <div className="card glass card-bordered card-compact w-80">
       <div className="card-body">
-        <h2 className="card-title text-cyan-200">Link</h2>
+        <h2 className="text-base-0 card-title">{linkName}</h2>
       </div>
     </div>
   );
