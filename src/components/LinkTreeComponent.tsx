237ac92d9631eb5f3d56a507173f81ec9e185d06
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2022-10-13 15:23:37 +0530

    feat: map `Links` to `LinkTree`

--- src/components/LinkTreeComponent.tsx
+++ src/components/LinkTreeComponent.tsx
@@ -1,18 +1,19 @@
 import { CardComponent } from "./CardComponent";
+import { Links } from "../utils/links";
 
 export const LinkTreeComponent = () => {
   return (
     <div className="flex h-screen items-center justify-center">
       <ul className="steps steps-vertical">
-        <li className="step step-primary mb-10 text-cyan-50">
-          <CardComponent />
-        </li>
-        <li className="step step-primary mb-10 text-cyan-50">
-          <CardComponent />
-        </li>
-        <li className="step step-primary mb-10 text-cyan-50">
-          <CardComponent />
-        </li>
+        {Links?.map((item) => {
+          const { id, linkName, linkURL } = item;
+
+          return (
+            <li key={id} className="step step-primary mb-10 text-cyan-50">
+              <CardComponent id={id} linkName={linkName} linkURL={linkURL} />
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
