527a6de3c521cf62a6a68dbaab879c205f17b300
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2022-10-21 00:35:25 +0530

    feat: add link numbers

--- src/components/CardComponent.tsx
+++ src/components/CardComponent.tsx
@@ -3,13 +3,13 @@ import { LinkType } from "../utils/links";
 import { motion } from "framer-motion";
 
 export const CardComponent = (props: LinkType) => {
-  const { linkName, linkURL } = props;
+  const { id, linkName, linkURL } = props;
 
   return (
-    <div className="card glass card-bordered w-80 items-center">
+    <div className="card glass card-bordered w-80 items-center justify-evenly">
       <div className="card-title">
         <h2 className="text-base-0 btn-ghost card-title bg-gradient-to-r from-violet-200 to-cyan-200 bg-clip-text font-mono text-2xl font-extrabold tracking-wide text-transparent subpixel-antialiased">
-          {linkName}
+          {id}. {linkName}
         </h2>
         <div className="card-actions">
           <motion.div
