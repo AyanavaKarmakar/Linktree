6f25e9a1388f602c54547cd7b494f1bc75f3583d
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2022-11-02 17:16:16 +0530

    feat(header): use text transition for header text + styles

---
+++ src/components/HeaderTransition.tsx
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react";
+import TextTransition, { presets } from "react-text-transition";
+
+const TEXTS = ["LINKTREE", "Ayanava Karmakar"];
+
+export const HeaderTransition = () => {
+  const [index, setIndex] = useState(0);
+  const INTERVAL_MS = 5000;
+
+  useEffect(() => {
+    const intervalId = setInterval(
+      () => setIndex((index) => index + 1),
+      INTERVAL_MS
+    );
+    return () => clearTimeout(intervalId);
+  }, []);
+
+  return (
+    <article className="prose prose-xl dark:prose-invert m-5">
+      <h1 className="font-extrabold capitalize tracking-wide text-cyan-50 underline underline-offset-8 decoration-dotted decoration-cyan-100">
+        <TextTransition springConfig={presets.gentle} direction={"down"}>
+          {TEXTS[index % TEXTS.length]}
+        </TextTransition>
+      </h1>
+    </article>
+  );
+};
