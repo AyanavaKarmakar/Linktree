6c203624da8beaebe097dd4dc5345aa450c05379
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2022-11-02 19:34:08 +0530

    chore(./utils/links): add link object + zod validation

---
+++ src/utils/Links.ts
@@ -0,0 +1,32 @@
+import { z } from "zod";
+
+const LinkValidator = z.object({
+  id: z.number().positive(),
+  linkName: z.string().min(3),
+  linkUrl: z.string().url().startsWith("https://"),
+});
+
+export type LinkType = z.infer<typeof LinkValidator>;
+
+export const Links: LinkType[] = [
+  {
+    id: 1,
+    linkName: "GitHub",
+    linkUrl: "https://github.com/AyanavaKarmakar",
+  },
+  {
+    id: 2,
+    linkName: "LinkedIn",
+    linkUrl: "https://www.linkedin.com/in/ayanava-karmakar/",
+  },
+  {
+    id: 3,
+    linkName: "Guestbook",
+    linkUrl: "https://ayanava-karmakar-guestbook.vercel.app/",
+  },
+  {
+    id: 4,
+    linkName: "Website",
+    linkUrl: "https://ayanava-karmakar.vercel.app/",
+  },
+];
