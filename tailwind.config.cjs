f25745280d73197e41eb9b6248eeec241ba7466c
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2022-11-02 16:36:57 +0530

    chore: use default font

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,9 +3,6 @@ module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {},
-    fontFamily: {
-      custom: ["ubuntu-mono", "Ubuntu", "sans-serif"],
-    },
   },
   plugins: [require("@tailwindcss/typography")],
 };
