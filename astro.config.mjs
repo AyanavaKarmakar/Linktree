23c10fb80b5e6ea91b7ee0ad1cd30dea9ca1f730
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2022-11-03 00:17:05 +0530

    chore: github pages deploy prep

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://AyanavaKarmakar.github.io",
+  base: "/LinkTree",
   integrations: [tailwind()],
 });
